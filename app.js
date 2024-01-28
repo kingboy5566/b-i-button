7db20d349efadf50c0fbb326e9a2a4d2424d860e
muhammad45401 <saadjani3333@gmail.com>
2024-01-28 15:49:31 +0500

    text-editir

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function itilic(){
+    document.getElementById("text1").className="itc"
+}
+function bold(){
+    document.getElementById("text1").className="bold"
+}
+function underline(){
+    document.getElementById("text1").className="under"
+}
+function center(){
+    document.getElementById("text1").className="center"
+}
+function jst(){
+    document.getElementById("text1").className="jst"
+}
+function jleft(){
+    document.getElementById("text1").className="jleft"
+}
+function jright(){
+    document.getElementById("text1").className="jright"
+}
